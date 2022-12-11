c9f4ca126ffb1f9470fd3babcff256c5fcb33630
SangeethaShanmugm <sanjuriya22@gmail.com>
2022-12-11 11:08:05 +0530

    added yup

--- src/BasicForm.js
+++ src/BasicForm.js
@@ -1,8 +1,23 @@
 import { useFormik } from "formik";
+import * as yup from "yup";
+
+const formValidationSchema = yup.object({
+  email: yup
+    .string()
+    .min(5, "Need a longer email😆")
+    .required("Why not fill the email?😉")
+    .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i),
+  password: yup
+    .string()
+    .min(8, "Need a longer password😆")
+    .max(12, "Too much Password")
+    .required("Why not fill the password?😉"),
+});
 
 export function BasicForm() {
   const formik = useFormik({
     initialValues: { email: "cool", password: "abc" },
+    validationSchema: formValidationSchema,
     onSubmit: (values) => {
       console.log("onSubmit", values);
     },
@@ -15,17 +30,27 @@ export function BasicForm() {
         name="email"
         value={formik.values.email}
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         type="email"
         placeholder="Email"
       />
+      <br />
+      {formik.touched.email && formik.errors.email ? formik.errors.email : ""}
+      <br />
       <input
         id="password"
         name="password"
         value={formik.values.password}
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         type="password"
         placeholder="Password"
       />
+      <br />
+      {formik.touched.password && formik.errors.password
+        ? formik.errors.password
+        : ""}
+      <br />
       <button type="submit">Submit</button>
     </form>
   );
